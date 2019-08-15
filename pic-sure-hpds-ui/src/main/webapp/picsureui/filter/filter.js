9bd6d900ca7f6f419df65fa4f0f4dda792355b93
Yu Zhang <yu_zhang@hms.harvard.edu>
2019-08-15 15:29:23 -0400

    typo

--- pic-sure-hpds-ui/src/main/webapp/picsureui/filter/filter.js
+++ pic-sure-hpds-ui/src/main/webapp/picsureui/filter/filter.js
@@ -251,7 +251,7 @@ define(["picSure/ontology", "text!filter/searchHelpTooltip.hbs", "output/outputP
 						isValid = false;
 					}
 				}
-				if (Number(constrainValueOne) > Number(constrainValueTwo){
+				if (Number(constrainValueOne) > Number(constrainValueTwo)){
 					$('.constrain-value-one', this.$el).addClass("field-invalid");
 					$('.constrain-value-two', this.$el).addClass("field-invalid");
 					isValid = false;
