504f9cca1b1d6c8921fb29a5b85616ae83a6c8e2
Jason Stedman <namdets@gmail.com>
2020-02-24 13:17:13 -0500

    formatting

--- pic-sure-hpds-ui/src/main/webapp/picsureui/common/startup.js
+++ pic-sure-hpds-ui/src/main/webapp/picsureui/common/startup.js
@@ -35,20 +35,20 @@ define(["filter/filterList", "header/header", "footer/footer", "text!../settings
 			            session.queryTemplate = response.queryTemplate;
 			            sessionStorage.setItem("session", JSON.stringify(session));
 
-						$('body').append(HBS.compile(layoutTemplate)(JSON.parse(settings)));
-						var headerView = header.View;
-						headerView.render();
-						$('#header-content').append(headerView.$el);
-						var footerView = footer.View;
-						footerView.render();
-						$('#footer-content').append(footerView.$el);
-						filterList.init();
-						var outputPanel = output.View;
-						outputPanel.render();
-						$('#query-results').append(outputPanel.$el);
+			            $('body').append(HBS.compile(layoutTemplate)(JSON.parse(settings)));
+			            var headerView = header.View;
+			            headerView.render();
+			            $('#header-content').append(headerView.$el);
+			            var footerView = footer.View;
+			            footerView.render();
+			            $('#footer-content').append(footerView.$el);
+			            filterList.init();
+			            var outputPanel = output.View;
+			            outputPanel.render();
+			            $('#query-results').append(outputPanel.$el);
 						
-						var query = queryBuilder.createQuery({});
-						outputPanel.update(query);
+			            var query = queryBuilder.createQuery({});
+			            outputPanel.update(query);
 			        }.bind(this),
 			        error: function (response) {
 			        	if (response.status == 401) {
