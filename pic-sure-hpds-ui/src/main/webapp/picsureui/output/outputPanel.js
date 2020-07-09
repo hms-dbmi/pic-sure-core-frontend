58cbc2c318cb33eb26ad1be8934972e58d8a470a
Nathan Chu <chu.nathan@gmail.com>
2020-07-09 14:31:41 -0400

    improve formatting, remove unused function

--- pic-sure-hpds-ui/src/main/webapp/picsureui/output/outputPanel.js
+++ pic-sure-hpds-ui/src/main/webapp/picsureui/output/outputPanel.js
@@ -24,108 +24,104 @@ define(["text!../settings/settings.json","common/spinner", "output/dataSelection
 		}
 	});
 
-	var mapPuiForResource = (typeof overrides.mapPuiForResource == 'function') ?
-			overrides.mapPuiForResource
-			: function(pui, picsureInstance){
-				return pui.replace(/(\/[\w-]+){4}/, picsureInstance.basePui);
-			};
+	var outputView = BB.View.extend({
+		ontology: ontology,
+		initialize: function(){
+			this.template = HBS.compile(outputTemplate);
+			overrides.renderOverride ? this.render = overrides.renderOverride.bind(this) : undefined;
+			overrides.update ? this.update = overrides.update.bind(this) : undefined;
+			HBS.registerHelper("outputPanel_obfuscate", function(count){
+				if(count < 10 && false){
+					return "< 10";
+				} else {
+					return count;
+				}
+			});
+		},
+		events:{
+			"click #select-btn": "select"
+		},
+		select: function(event){
+			
+			this.model.set('spinning', true);
+			if(!this.dataSelection){
+				var query = JSON.parse(JSON.stringify(this.model.get("query")));
+				this.dataSelection = new dataSelection({query:query});
+				$("#concept-tree-div",this.$el).append(this.dataSelection.$el);
+				this.model.set("spinning", false);
+				this.dataSelection.render();
+			}
+		},
+		totalCount: 0,
+		tagName: "div",
+		update: function(incomingQuery){
+			this.model.set("totalPatients",0);
 
-			var outputView = overrides.viewOverride ? overrides.viewOverride : BB.View.extend({
-				ontology: ontology,
-				initialize: function(){
-					this.template = HBS.compile(outputTemplate);
-					overrides.renderOverride ? this.render = overrides.renderOverride.bind(this) : undefined;
-					overrides.update ? this.update = overrides.update.bind(this) : undefined;
-					HBS.registerHelper("outputPanel_obfuscate", function(count){
-						if(count < 10 && false){
-							return "< 10";
-						} else {
-							return count;
-						}
-					});
-				},
-				events:{
-					"click #select-btn": "select"
-				},
-				select: function(event){
-					
-					this.model.set('spinning', true);
-					if(!this.dataSelection){
-						var query = JSON.parse(JSON.stringify(this.model.get("query")));
-						this.dataSelection = new dataSelection({query:query});
-						$("#concept-tree-div",this.$el).append(this.dataSelection.$el);
-						this.model.set("spinning", false);
-						this.dataSelection.render();
-					}
-				},
-				totalCount: 0,
-				tagName: "div",
-				update: function(incomingQuery){
-					this.model.set("totalPatients",0);
+			this.model.spinAll();
+			this.render();
 
-					this.model.spinAll();
-					this.render();
+			// make a safe deep copy of the incoming query so we don't modify it
+			var query = JSON.parse(JSON.stringify(incomingQuery));
+			query.resourceUUID = JSON.parse(settings).picSureResourceId;
+			query.resourceCredentials = {};
+			query.query.expectedResultType="COUNT";
+			this.model.set("query", query);
 
-					// make a safe deep copy of the incoming query so we don't modify it
-					var query = JSON.parse(JSON.stringify(incomingQuery));
-					query.resourceUUID = JSON.parse(settings).picSureResourceId;
-					query.resourceCredentials = {};
-					query.query.expectedResultType="COUNT";
-					this.model.set("query", query);
+			if(this.dataSelection){
+				this.dataSelection.updateQuery(query);
+				this.dataSelection.render();
+			}
 
-					if(this.dataSelection){
-						this.dataSelection.updateQuery(query);
-						this.dataSelection.render();
-					}
+			var dataCallback = function(result){
+				this.model.set("totalPatients", parseInt(result));
+				this.model.set("spinning", false);
+				this.model.set("queryRan", true);
+				this.render();
+			}.bind(this);
 
-					var dataCallback = function(result){
-						this.model.set("totalPatients", parseInt(result));
-						this.model.set("spinning", false);
-						this.model.set("queryRan", true);
-						this.render();
-					}.bind(this);
+			var errorCallback = function(message){
+				this.model.set("spinning", false);
+                                        this.model.set("queryRan", true);
+                                        this.render();
+				$("#patient-count").html(message);
+			}.bind(this);
 
-					var errorCallback = function(message){
-						this.model.set("spinning", false);
-                                                this.model.set("queryRan", true);
-                                                this.render();
-						$("#patient-count").html(message);
-					}.bind(this);
-
-					$.ajax({
-					 	url: window.location.origin + "/picsure/query/sync",
-					 	type: 'POST',
-					 	headers: {"Authorization": "Bearer " + JSON.parse(sessionStorage.getItem("session")).token},
-					 	contentType: 'application/json',
-					 	data: JSON.stringify(query),
-					 	success: function(response){
-					 		dataCallback(response);
-					 	},
-					 	error: function(response){
-							if (response.status === 401) {
-								sessionStorage.clear();
-								window.location = "/";
-							} else {
-								response.responseText = "<h4>"
-									+ overrides.outputErrorMessage ? overrides.outputErrorMessage : "There is something wrong when processing your query, please try it later, if this repeats, please contact admin."
-									+ "</h4>";
-						 		errorCallback(response.responseText);//console.log("error");console.log(response);
-							}
-						}
-					});
-				},
-				render: function(){
-					var context = this.model.toJSON();
-					this.$el.html(this.template(Object.assign({},context , overrides)));
-					if(this.dataSelection){
-						this.dataSelection.setElement($("#concept-tree-div",this.$el));
-						this.dataSelection.render();
+			$.ajax({
+			 	url: window.location.origin + "/picsure/query/sync",
+			 	type: 'POST',
+			 	headers: {"Authorization": "Bearer " + JSON.parse(sessionStorage.getItem("session")).token},
+			 	contentType: 'application/json',
+			 	data: JSON.stringify(query),
+			 	success: function(response){
+			 		dataCallback(response);
+			 	},
+			 	error: function(response){
+					if (response.status === 401) {
+						sessionStorage.clear();
+						window.location = "/";
+					} else {
+						response.responseText = "<h4>"
+							+ overrides.outputErrorMessage ? overrides.outputErrorMessage : "There is something wrong when processing your query, please try it later, if this repeats, please contact admin."
+							+ "</h4>";
+				 		errorCallback(response.responseText);//console.log("error");console.log(response);
 					}
 				}
 			});
-			return {
-				View : new (overrides.viewOverride ? overrides.viewOverride : outputView)({
-					model: new (overrides.modelOverride ? overrides.modelOverride : outputModel)()
-				})
+		},
+		render: function(){
+			var context = this.model.toJSON();
+			this.$el.html(this.template(Object.assign({},context , overrides)));
+			if(this.dataSelection){
+				this.dataSelection.setElement($("#concept-tree-div",this.$el));
+				this.dataSelection.render();
 			}
+		}
+	});
+	
+	
+	return {
+		View : new (overrides.viewOverride ? overrides.viewOverride : outputView)({
+			model: new (overrides.modelOverride ? overrides.modelOverride : outputModel)()
+		})
+	}
 });
