7509ec030fcab360b88bc98d90c163edb8a29539
Ryan Amari <ramari16@gmail.com>
2021-06-17 14:49:35 -0400

    ALS-1642: Move variant explorer code from udn into base repo

---
+++ pic-sure-hpds-ui/src/main/webapp/picsureui/common/config.js
@@ -0,0 +1,6 @@
+define([], function(){
+    const VariantExplorerStatusEnum = Object.freeze({"disabled":"disabled", "enabled":"enabled", "aggregate":"aggregate"});
+    return {
+        VariantExplorerStatusEnum: VariantExplorerStatusEnum
+    };
+});
--- pic-sure-hpds-ui/src/main/webapp/picsureui/output/outputPanel.js
+++ pic-sure-hpds-ui/src/main/webapp/picsureui/output/outputPanel.js
@@ -1,7 +1,13 @@
-define(["jquery", "output/dataSelection", "text!output/outputPanel.hbs", "picSure/ontology", "backbone", "handlebars", "overrides/outputPanel", "common/transportErrors"],
-		function($,  dataSelection, outputTemplate, ontology, BB, HBS, overrides, transportErrors){
+define(["jquery", "output/dataSelection", "text!output/outputPanel.hbs", "picSure/ontology", "backbone", "handlebars",
+		"overrides/outputPanel", "common/transportErrors", "common/config", "text!output/variantTable.hbs",
+		"text!options/modal.hbs", "picSure/settings"],
+		function($,  dataSelection, outputTemplate, ontology, BB, HBS,
+				 overrides, transportErrors, config, variantTableTemplate,
+				 modalTemplate, settings){
 
 	var outputModel = overrides.modelOverride ? overrides.modelOverride : BB.Model.extend({	});
+
+	const variantExplorerStatus = overrides.variantExplorerStatus ? overrides.variantExplorerStatus : config.VariantExplorerStatusEnum.disabled;
 
 	var outputView = overrides.viewOverride ? overrides.viewOverride : 
 		BB.View.extend({
@@ -17,10 +23,16 @@ define(["jquery", "output/dataSelection", "text!output/outputPanel.hbs", "picSur
 						return count;
 					}
 				});
+
+				if (variantExplorerStatus === config.VariantExplorerStatusEnum.enabled) {
+					this.variantTableTemplate = HBS.compile(variantTableTemplate);
+					this.modalTemplate = HBS.compile(modalTemplate);
+				}
 			},
 			
 			events:{
-				"click #select-btn": "select"
+				"click #select-btn": "select",
+				"click #variant-data-btn" : "variantdata",
 			},
 			select: function(event){
 				if(this.model.get("queryRan")){
@@ -50,6 +62,19 @@ define(["jquery", "output/dataSelection", "text!output/outputPanel.hbs", "picSur
 					this.dataSelection.updateQuery(this.model.baseQuery);
 					this.dataSelection.render();
 				}
+
+				if(variantExplorerStatus === config.VariantExplorerStatusEnum.enabled
+					&& this.model.baseQuery
+					&& this.model.baseQuery.query.variantInfoFilters.length > 0){
+					_.each(this.model.baseQuery.query.variantInfoFilters, function(variantHolder){
+						if(Object.keys(variantHolder.categoryVariantInfoFilters).length != 0 ||
+							Object.keys(variantHolder.numericVariantInfoFilters).length != 0){
+							$("#variant-data-btn").removeClass("hidden");
+							return false;
+						}
+					});
+				}
+
     			this.delegateEvents();
 			},
 			errorCallback: function(message){
@@ -93,6 +118,128 @@ define(["jquery", "output/dataSelection", "text!output/outputPanel.hbs", "picSur
 					this.dataSelection.setElement($("#concept-tree-div",this.$el));
 					this.dataSelection.render();
 				}
+			},
+			// Check the number of variants in the query and show a modal if valid.
+			variantdata: function(event){
+				if (variantExplorerStatus === config.VariantExplorerStatusEnum.enabled) {
+					this.model.set('spinning', true);
+					this.render();
+					// make a safe deep copy of the incoming query so we don't modify it
+					var query = JSON.parse(JSON.stringify(this.model.baseQuery));
+
+					//this query type counts the number of variants described by the query.
+					query.query.expectedResultType="VARIANT_COUNT_FOR_QUERY";
+
+					$.ajax({
+						url: window.location.origin + "/picsure/query/sync",
+						type: 'POST',
+						headers: {"Authorization": "Bearer " + JSON.parse(sessionStorage.getItem("session")).token},
+						contentType: 'application/json',
+						data: JSON.stringify(query),
+						dataType: 'text',
+						success: function(response){
+
+							//If there are fewer variants than the limit, show the modal
+							maxVariantCount =  settings.maxVariantCount ? settings.maxVariantCount : 1000;
+
+							responseJson = JSON.parse(response);
+							// use this to test vs current udn production
+							//var responseJson = {count: parseInt(response)}
+
+							if( responseJson.count == 0 ){
+								this.showBasicModal("Variant Data", "No Variant Data Available.  " + responseJson.message);
+							} else if( responseJson.count <= maxVariantCount ){
+								this.showVariantDataModal(query);
+							} else {
+								this.showBasicModal("Variant Data", "Too many variants!  Found " + parseInt(response) + ", but cannot display more than " + maxVariantCount + " variants.")
+							}
+						}.bind(this),
+						error: function(response){
+							this.model.set('spinning', false);
+							this.render();
+							console.log("ERROR: " + response);
+						}
+					});
+				}
+			},
+
+			showBasicModal: function(titleStr, content){
+				$("#modal-window").html(this.modalTemplate({title: titleStr}));
+				$(".close").click(function(){
+					$("#modalDialog").hide();
+				});
+
+				$("#modalDialog").show();
+				$(".modal-body").html(content);
+				this.model.set('spinning', false);
+				this.render();
+			},
+			//this takes a parsed query object and gets a list of variant data & zygosities from the HPDS
+			//and creates a modal table to display it
+			showVariantDataModal: function(query){
+
+				//we expect an object that has already been parsed/copied from the model.
+				//update the result type to get the variant data
+				query.query.expectedResultType="VCF_EXCERPT";
+
+				$.ajax({
+					url: window.location.origin + "/picsure/query/sync",
+					type: 'POST',
+					headers: {"Authorization": "Bearer " + JSON.parse(sessionStorage.getItem("session")).token},
+					contentType: 'application/json',
+					data: JSON.stringify(query),
+					dataType: 'text',
+					success: function(response){
+//				 		console.log(response);
+
+						//default message if no data
+						variantHtml = "No Variant Data Available"
+						output = {};
+
+						if(response.length > 0){
+							//each line is TSV
+							var lines = response.split("\n");
+
+							headers = lines[0].split("\t");
+							output["headers"] = headers;
+							output["variants"] = []
+							//read the tsv lines into an object that we can sent to Handlebars template
+							for(i = 1; i < lines.length; i++){
+								variantData = {};
+								values = lines[i].split("\t");
+								for(j=0; j < values.length; j++){
+									variantData[headers[j]] = values[j];
+								}
+								output["variants"].push(variantData);
+							}
+							//render the template!
+							variantHtml = this.variantTableTemplate(output);
+						}
+
+						//lines ends up with a trailing empty object; strip that and the header row for the count
+						$("#modal-window").html(this.modalTemplate({title: "Variant Data: " + (lines.length - 2) + " variants found"}));
+						$(".close").click(function(){
+							$("#modalDialog").hide();
+						});
+						$("#modalDialog").show();
+						$(".modal-body").html(variantHtml);
+
+						//now add a handy download link!
+						$(".modal-header").append("<a id='variant-download-btn'>Download Variant Data</a>");
+						responseDataUrl = URL.createObjectURL(new Blob([response], {type: "octet/stream"}));
+						$("#variant-download-btn", $(".modal-header")).off('click');
+						$("#variant-download-btn", $(".modal-header")).attr("href", responseDataUrl);
+						$("#variant-download-btn", $(".modal-header")).attr("download", "variantData.tsv");
+						this.model.set('spinning', false);
+						this.render();
+					}.bind(this),
+					error: function(response){
+						console.log("ERROR: " + response);
+						this.model.set('spinning', false);
+						this.render();
+					}
+				});
+
 			}
 		});
 	
