549ec94029348f7e29331d4ce4638022d7ca9bdd
Jason Stedman <namdets@gmail.com>
2021-07-21 19:56:12 -0400

    initial pass at datatables integration and spinner

--- pic-sure-hpds-ui/src/main/webapp/picsureui/common/styles.js
+++ pic-sure-hpds-ui/src/main/webapp/picsureui/common/styles.js
@@ -1,8 +1,8 @@
-define(["jquery", "text!styles.css", "text!bootstrapStyles", "text!treeviewStyles", "text!overrides/styles.css"], 
-		function($, styles, bootstrapStyles, bootstrapTreeviewStyles, overrides){
+define(["jquery", "text!styles.css", "text!bootstrapStyles", "text!treeviewStyles", "text!datatablesStyles", "text!overrides/styles.css"], 
+		function($, styles, bootstrapStyles, bootstrapTreeviewStyles, datatablesStyles, overrides){
 	$('head').append("<style></style>");
 	$('head style').html(
 			bootstrapStyles.replace(new RegExp('\.\./fonts/', 'g'),
 					'/picsureui/webjars/bootstrap/3.4.1/fonts/')
-					+ bootstrapTreeviewStyles + styles + overrides);
+					+ bootstrapTreeviewStyles + datatablesStyles + styles + overrides);
 });
