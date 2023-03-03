39dc03009c60a37699515da41a9bddc626570300
GeorgeC <gcolon021@gmail.com>
2023-03-03 16:03:21 -0500

    [ALS-1078] Added two new functions to both pages: when a user assigns any admin role, the 'PIC-SURE user' role should also be assigned. Similarly, when a user removes the 'PIC-SURE user' role, they should be removed from all admin roles.

--- pic-sure-hpds-ui/src/main/webapp/picsureui/psamaui/user/addUser.js
+++ pic-sure-hpds-ui/src/main/webapp/picsureui/psamaui/user/addUser.js
@@ -1,7 +1,7 @@
 define(["backbone", "handlebars", "user/connections", "picSure/userFunctions", "picSure/picsureFunctions", "text!user/addUser.hbs", "text!user/addUserConnectionForm.hbs", "util/notification"],
 		function(BB, HBS, connections, userFunctions, picsureFunctions, template, connectionTemplate, notification){
 	let view = BB.View.extend({
-		initialize: function(opts){
+		initialize: function(opts) {
 			this.connectionTemplate = HBS.compile(connectionTemplate);
 			this.template = HBS.compile(template);
 			this.managementConsole = opts.managementConsole;
@@ -12,6 +12,8 @@ define(["backbone", "handlebars", "user/connections", "picSure/userFunctions", "
 			"input #email": "validateEmail",
 			"click #save-user-button":   "saveUserAction",
 			"click #cancel-user-button": "closeDialog",
+			"click input[name*='Admin']": "checkUserRoleOnAdminRoleChecked",
+			"click input[name='PIC-SURE User']": "uncheckAdminRoleOnUnchecked"
 		},
 		validateEmail: function(event){
 	        let emailReg = /(?:[a-zA-Z0-9!#$%&'*+/=?^_{|}~-]+(?:\.[a-zA-Z0-9!#$%&'*+/=?^_{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?\.)+[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-zA-Z0-9-]*[a-zA-Z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
@@ -43,8 +45,9 @@ define(["backbone", "handlebars", "user/connections", "picSure/userFunctions", "
 			let general_metadata = {};
 
 			user.connection = {
-						id: this.$('input[name=connectionId]').val()
-				};
+				id: this.$('input[name=connectionId]').val()
+			};
+
 			// use the connection field to determine if this is a new or existing user.
 			if(user.connection.id){
 				//existing - read meta fields from inputs
@@ -52,9 +55,9 @@ define(["backbone", "handlebars", "user/connections", "picSure/userFunctions", "
 				user.subject = this.$('input[name=subject]').val();
 				//this includes the user uuid
 				_.each($('#required-fields input[type=text]'), function(entry){
-					general_metadata[entry.name] = entry.value
+					general_metadata[entry.name] = entry.value;
 				});
-				user.email = general_metadata["email"] ? general_metadata["email"] : email; // synchronize email with metadata
+				user.email = general_metadata.email ? general_metadata.email : email; // synchronize email with metadata
 			} else {
 				//new user - we have a different connection and email input to read, and no auth0 data
 				user.connection = {
@@ -70,7 +73,7 @@ define(["backbone", "handlebars", "user/connections", "picSure/userFunctions", "
 			let roles = [];
 			_.each(this.$('input:checked'), function (checkbox) {
 				roles.push({uuid: checkbox.value});
-			})
+			});
 			user.roles = roles;
 			userFunctions.createOrUpdateUser([user], user.uuid == null ? 'POST' : 'PUT', function(result) {
 				console.log(result);
@@ -96,12 +99,31 @@ define(["backbone", "handlebars", "user/connections", "picSure/userFunctions", "
 			this.model.unset("selectedUser");
 			$(".close").click();
 		},
-		render: function(){
+		checkUserRoleOnAdminRoleChecked: function (event) {
+			console.debug("checkUserRoleOnAdminRoleChecked");
+			let userRoleCheckbox = $('input[name="PIC-SURE User"]');
+
+			let isAdminChecked = event.target.checked;
+			if (isAdminChecked) {
+				userRoleCheckbox.prop('checked', isAdminChecked);
+			}
+		},
+		uncheckAdminRoleOnUnchecked: function (event) {
+			console.debug("uncheckAdminRoleOnUnchecked");
+			let adminRoleCheckbox = $('input[name*="Admin"]');
+			let isUserRoleChecked = event.target.checked;
+
+			// If the user role has been unchecked we must uncheck the admin roles
+			if (!isUserRoleChecked) {
+				adminRoleCheckbox.prop('checked', isUserRoleChecked);
+			}
+		},
+		render: function() {
 			// load available connections first
             this.getConnections(function (connections) {
                 this.connections = connections;
                 this.$el.html(this.template({connections: this.connections}));
-                this.renderConnectionForm({target:{value:this.connections[0].id}})
+                this.renderConnectionForm({target:{value:this.connections[0].id}});
             }.bind(this));
 		}
 	});
