c048e14a9b6a9bb47abc5c4878ca7ce4898cbe30
James <Jamestp19@gmail.com>
2023-08-23 17:46:42 -0400

    Decouple variant explorer from output panel

--- pic-sure-hpds-ui/src/main/webapp/picsureui/output/variantExplorer.js
+++ pic-sure-hpds-ui/src/main/webapp/picsureui/output/variantExplorer.js
@@ -2,120 +2,93 @@ define(["jquery", "underscore", "datatables.net", "backbone", "handlebars", "tex
         "common/config", "common/spinner"],
     function($, _, datatables, BB, HBS, variantTableTemplate, modalTemplate, settings,
              config, spinner){
-
+        const maxVariantCount =  settings.maxVariantCount ? settings.maxVariantCount : 1000;
+        let createDownloadLink = function(response){
+            //now add a handy download link!
+            $(".modal-header").append("<a id='variant-download-btn'>Download Variant Data</a>");
+            const responseDataUrl = URL.createObjectURL(new Blob([response], {type: "octet/stream"}));
+            $("#variant-download-btn", $(".modal-header")).off('click');
+            $("#variant-download-btn", $(".modal-header")).attr("href", responseDataUrl);
+            $("#variant-download-btn", $(".modal-header")).attr("download", "variantData.tsv");
+        }
         let variantExplorerView = BB.View.extend({
-            initialize: function() {
+            initialize: function(opts) {
+                this.baseQuery = opts.query;
+                this.dataErrorMsg = opts.errorMsg ? opts.errorMsg : "There was an error loading the data";
                 this.variantTableTemplate = HBS.compile(variantTableTemplate);
-                this.modalTemplate = HBS.compile(modalTemplate);
             },
-            events: {
-                "click #variant-data-btn" : "variantdata"
-            },
-            updateQuery: function(query) {
-                this.baseQuery = query;
-                this.displayVariantButton();
-            },
-            displayVariantButton: function() {
-                var showVariantButton = false;
-                if(this.baseQuery && this.baseQuery.query.variantInfoFilters.length > 0){
-                    _.each(this.baseQuery.query.variantInfoFilters, function(variantHolder){
-                        if(Object.keys(variantHolder.categoryVariantInfoFilters).length != 0 ||
-                            Object.keys(variantHolder.numericVariantInfoFilters).length != 0){
-                            showVariantButton = true;
+            createDataTable: function(){
+                let renderComplete = $.Deferred();
+                $('#vcfData').DataTable( {
+                    data: this.variantData["variants"],
+                    columns: _.map(this.variantData['headers'],(header)=>{
+                        return {title:header }
+                    }),columnDefs: [
+                        {
+                            targets: '_all',
+                            className: 'dt-center'
+                        },
+                        {
+                            targets: [12,13],
+                            visible: false
                         }
-                    });
-                }
-                if (showVariantButton) {
-                    $("#variant-data-container").removeClass("hidden");
-                } else {
-                    $("#variant-data-container").addClass("hidden");
-                }
+                      ],
+                    deferRender: true,
+                    drawCallback: function(){
+                        renderComplete.resolve();
+                    }.bind(this)
+                } );
+                return renderComplete;
             },
-            // Check the number of variants in the query and show a modal if valid.
-            variantdata: function(event){
-                // make a safe deep copy of the incoming query so we don't modify it
-                var query = JSON.parse(JSON.stringify(this.baseQuery));
-
-                //this query type counts the number of variants described by the query.
-                query.query.expectedResultType="VARIANT_COUNT_FOR_QUERY";
-
-                this.renderComplete = $.Deferred();
-                spinner.medium(this.renderComplete, "#variant-spinner", "spinner-medium spinner-medium-center ");
-
-                $.ajax({
+            getVariantCount: function(){
+                let deepCopyQuery = JSON.parse(JSON.stringify(this.baseQuery));
+                deepCopyQuery.query.expectedResultType="VARIANT_COUNT_FOR_QUERY";
+                return $.ajax({
                     url: window.location.origin + "/picsure/query/sync",
                     type: 'POST',
                     headers: {"Authorization": "Bearer " + JSON.parse(sessionStorage.getItem("session")).token},
                     contentType: 'application/json',
-                    data: JSON.stringify(query),
+                    data: JSON.stringify(deepCopyQuery),
                     dataType: 'text',
                     success: function(response){
-
-                        //If there are fewer variants than the limit, show the modal
-                        maxVariantCount =  settings.maxVariantCount ? settings.maxVariantCount : 1000;
-
-                        let responseJson = JSON.parse(response);
-
-                        if( responseJson.count == 0 ){
-                            this.showBasicModal("Variant Data", "No Variant Data Available.  " + responseJson.message);
-                            this.renderComplete.resolve();
-                        } else if( responseJson.count <= maxVariantCount ){
-                            this.showVariantDataModal(query);
-                        } else {
-                            this.showBasicModal("Variant Data", "Too many variants!  Found " + responseJson.count + ", but cannot display more than " + maxVariantCount + " variants.")
-                            this.renderComplete.resolve();
+                        if (response) {
+                            let responseJson = JSON.parse(response);
+                            if (responseJson.count !== undefined) {
+                                this.variantCount = responseJson.count;
+                                $(".modal-title").html("Variant Data: " + (responseJson.count - 2) + " variants found");
+                                return;
+                            }
                         }
                     }.bind(this),
                     error: function(response){
-                        this.render();
                         console.log("ERROR: " + response);
-                        this.renderComplete.resolve();
                     }.bind(this)
                 });
             },
-            showBasicModal: function(titleStr, content){
-                $("#modal-window").html(this.modalTemplate({title: titleStr}));
-                $(".close").click(function(){
-                    $("#modalDialog").hide();
-                });
-                $("#modalDialog").show();
-                $(".modal-body").html(content);
-                this.render();
-            },
-            //this takes a parsed query object and gets a list of variant data & zygosities from the HPDS
-            //and creates a modal table to display it
-            showVariantDataModal: function(query){
-
-                //we expect an object that has already been parsed/copied from the model.
-                //update the result type to get the variant data
+            getVariantData: function(){
+                let deepCopyQuery = JSON.parse(JSON.stringify(this.baseQuery));
                 if (settings.variantExplorerStatus === config.VariantExplorerStatusEnum.enabled) {
-                    query.query.expectedResultType="VCF_EXCERPT";
+                    deepCopyQuery.query.expectedResultType="VCF_EXCERPT";
                 } else if (settings.variantExplorerStatus === config.VariantExplorerStatusEnum.aggregate) {
-                    query.query.expectedResultType="AGGREGATE_VCF_EXCERPT";
+                    deepCopyQuery.query.expectedResultType="AGGREGATE_VCF_EXCERPT";
                 } else {
                     throw "variantExplorerStatus must be enabled or aggregate";
                 }
-
-                $.ajax({
+                return $.ajax({
                     url: window.location.origin + "/picsure/query/sync",
                     type: 'POST',
                     headers: {"Authorization": "Bearer " + JSON.parse(sessionStorage.getItem("session")).token},
                     contentType: 'application/json',
-                    data: JSON.stringify(query),
+                    data: JSON.stringify(deepCopyQuery),
                     dataType: 'text',
                     success: function(response){
-
-                        //default message if no data
-                        variantHtml = "No Variant Data Available"
-                        output = {};
-
+                        let output = {};
                         if(response.length > 0){
                             //each line is TSV
-                            var lines = response.split("\n");
-
+                            let lines = response.split("\n");
                             headers = lines[0].split("\t");
-                            output["headers"] = headers;
-                            output["variants"] = []
+                            output["headers"] = headers; 
+                            output["variants"] = [];
                             //read the tsv lines into an object that we can sent to Handlebars template
                             for(i = 1; i < lines.length; i++){
                                 values = lines[i].split("\t");
@@ -123,61 +96,64 @@ define(["jquery", "underscore", "datatables.net", "backbone", "handlebars", "tex
                                     output["variants"].push(values);
                                 }
                             }
+                            output = this.reorderColumns(output);
                         }
-
-                        //lines ends up with a trailing empty object; strip that and the header row for the count
-                        $("#modal-window").html(this.modalTemplate({title: "Variant Data: " + (lines.length - 2) + " variants found"}));
-                        $(".close").click(function(){
-                            $("#modalDialog").hide();
-                        });
-
-                        $("#modalDialog").show();
-                        $(".modal-body").css("overflow-x", "scroll")
-                        $(".modal-body").html("<style scoped>th{width:auto !important; }</style> <table id='vcfData' class='display stripe' ></table>");
-
-                        $('#vcfData').DataTable( {
-                            data: output["variants"],
-                            columns: _.map(output['headers'],(header)=>{
-                                return {title:header }
-                            }),columnDefs: [
-                                {
-                                    targets: '_all',
-                                    className: 'dt-center'
-                                },
-                                {
-                                    targets: [6, 11],
-                                    visible: false
-                                }
-                              ],
-                            deferRender: true,
-                            drawCallback: function(){
-                                this.renderComplete.resolve();
-                            }.bind(this)
-                        } );
-
-                        //now add a handy download link!
-                        $(".modal-header").append("<a id='variant-download-btn'>Download Variant Data</a>");
-                        responseDataUrl = URL.createObjectURL(new Blob([response], {type: "octet/stream"}));
-                        $("#variant-download-btn", $(".modal-header")).off('click');
-                        $("#variant-download-btn", $(".modal-header")).attr("href", responseDataUrl);
-                        $("#variant-download-btn", $(".modal-header")).attr("download", "variantData.tsv");
-                        this.render();
-                        this.renderComplete.resolve();
+                        this.variantData = output;
                     }.bind(this),
                     error: function(response){
+                        this.handleDataError(this.dataErrorMsg);
                         console.log("ERROR: " + response);
-                        this.render();
-                        this.renderComplete.resolve();
-                    }.bind(this)
+                    }.bind(this),
                 });
             },
+            handleDataError: function(errorMsg){
+                this.$el.html('<div class="variant-exporer-error"><i class="fa fa-TODO"></i><h4>Data failed to load</h4><br><p>'+errorMsg+'</p><div>');
+            },
+            handleSiteDisabled: function(){
+                this.$el.html('<div class="variant-exporer-error"><i class="fa fa-TODO"></i><h4>There is no data to display</h4><br><h5>The Variant Explorer is not enabled for this site</h5><div>');
+            },
+            reorderColumns: function(input){
+                const headersToMoveInOrder = ['CHROM', 'POSITION', 'REF', 'ALT', 'Patients with this variant in subset', 'Patients With this variant NOT in subset', 'Variant_consequence_calculated', 'Gene_with_variant', 'Variant_class', 'Variant_severity', 'Variant_frequency_in_gnomAD', 'Variant_frequency_as_text', 'AC', 'AN'];
+                // Create a map of header indexes for quick lookup
+                const headerIndexMap = {};
+                    input.headers.forEach((header, index) => {
+                    headerIndexMap[header] = index;
+                });
+                const reorderedHeaders = headersToMoveInOrder.concat(input.headers.filter(header => !headersToMoveInOrder.includes(header)));
+                //variants is an array of arrays so we need to reorder each variant array
+                const reorderedVariants = input.variants.map(variant => {
+                    const reorderedVariant = [];
+                    reorderedHeaders.forEach(header => {
+                        const headerIndex = headerIndexMap[header];
+                        if (headerIndex !== undefined) {
+                            reorderedVariant.push(variant[headerIndex]);
+                        }
+                    });
+                    return reorderedVariant;
+                });
+
+                return output = {
+                    headers: reorderedHeaders,
+                    variants: reorderedVariants
+                };
+            },
             render: function() {
+                this.$el.html(this.variantTableTemplate());
+                let loading = this.getVariantCount().then(() => {
+                    if (this.variantCount > maxVariantCount) {
+                        this.handleDataError('Too many variants!  Found ' + this.variantCount + ', but cannot display more than ' + settings.maxVariantCount + ' variants.');
+                    } else {
+                        return this.getVariantData().then(() => {
+                            if (settings.queryExportType && settings.queryExportType !== "EXPORT_DISABLED") {
+                                createDownloadLink();
+                            }
+                            return this.createDataTable();
+                        });
+                    }
+                });
+                spinner.medium(loading, "#variant-spinner", "spinner-medium spinner-medium-center");
             }
         });
-
-        return {
-            View: variantExplorerView,
-            Model: BB.Model.extend({	})
-        }
+        return variantExplorerView;
     }
 );
