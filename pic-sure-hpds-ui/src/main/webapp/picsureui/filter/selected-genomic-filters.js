8a7f486b4c12d659f7e8e8d2de70eb5a141f47c3
James <Jamestp19@gmail.com>
2023-09-25 16:10:58 -0400

    [ALS-4918] New search interface; enable genomic filter modal (#136)
    * Update search UI for better genomic searches
    
    * More search ui updates
    
    * fix sp, styles for baseline
    
    * Hides genomic filter button when no data, bug fix
    
    * Check if empty or undefined
    
    * :angry:
    
    * Update my bad boolean logic
    
    * make sure info cols are loaded
    
    * [ALS-5058]: Fix bug with selecting lots of items
    
    * Update reset warning
    
    * sanitize inputs
    
    * Fix encoding
    
    * ALS-1513
    
    * Fix old genomic filtering not working with new way
    
    * Fix ui issue and use arrow function
    
    * remove urlencode as its done later
    
    * Remove whitespace changes
    
    * oxford comma
    
    * spacing
    
    * remove unused function
    
    * Don't pass stuff when you don't need to

--- pic-sure-hpds-ui/src/main/webapp/picsureui/filter/selected-genomic-filters.js
+++ pic-sure-hpds-ui/src/main/webapp/picsureui/filter/selected-genomic-filters.js
@@ -8,11 +8,14 @@ define([
             this.template = HBS.compile(template);
             this.title = opts.title;
             this.description = opts.description;
-            this.clearButton = true;
+            this.clearButton = opts.clearButton;
             this.clearAction = opts.clearAction || this.clearLists;
+            this.editAction = opts.editAction || this.editLists;
+            this.editAction != undefined ? this.editButton = true : this.editButton = false;
         },
         events : {
             'click #selected-genomic-clear-btn' : 'clear',
+            'click #selected-genomic-edit-btn' : 'edit'
         },
         clear: function(){
             this.clearAction();
@@ -23,10 +26,22 @@ define([
             this.selectedConsequences = [];
             this.render();
         },
+        edit: function() {
+            this.editAction();
+        },
+        getCurrentFilter: function(){
+            return {variantInfoFilters: [{categoryVariantInfoFilters: 
+                    {
+                        Gene_with_variant: this.selectedVariants,
+                        Variant_frequency_as_text: this.selectedFrequencies,
+                        Variant_consequence_calculated: this.selectedConsequences
+                    }
+                }]};
+        },
         updateFilter: function(filter){
-            this.selectedVariants = filter.Gene_with_variant;
-            this.selectedFrequencies = filter.Variant_frequency_as_text;
-            this.selectedConsequences = filter.Variant_consequence_calculated;
+            this.selectedVariants = filter?.Gene_with_variant;
+            this.selectedFrequencies = filter?.Variant_frequency_as_text;
+            this.selectedConsequences = filter?.Variant_consequence_calculated;
             this.render();
         },
         render: function(){
