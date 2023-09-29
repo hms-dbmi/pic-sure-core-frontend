f2e05c98510809b29b13547e8aa3e27504cc28a9
gcolon021 <gcolon021@gmail.com>
2023-09-29 14:16:05 -0400

    [ALS-5107] Fix naming convention

--- pic-sure-hpds-ui/src/main/webapp/picsureui/header/bannerConfig.js
+++ pic-sure-hpds-ui/src/main/webapp/picsureui/header/bannerConfig.js
@@ -1,4 +1,4 @@
 // This is used to pull configuration from headerConfig.json
-define(["text!settings/bannerConfig.json"], function(settings){
+define(["text!settings/banner_config.json"], function(settings){
     return JSON.parse(settings);
 });
