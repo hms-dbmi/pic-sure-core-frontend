9ab7da2ac6aa6665a56a02fa03ce372292ec6cdd
Samantha <samantha.piatt@childrens.harvard.edu>
2023-11-03 12:06:35 -0400

    [ALS-4773] [ALS-5244] Refactoring the data-view modal to work on other pages and allow copying (#170)
    * Refactoring the data-view page and some styles

---
+++ pic-sure-hpds-ui/src/main/webapp/picsureui/overrides/dataset/utilities.js
@@ -0,0 +1,9 @@
+define([],  function(){
+    return {
+        /* List of overrides for query object formatting. */
+        format: {},
+
+        /* List of overrides for query object rendering. Used with format outputs. */
+        render: {}
+    };
+});
