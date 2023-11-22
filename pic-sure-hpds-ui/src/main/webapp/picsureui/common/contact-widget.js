c7b5972e61206f86e809e1d337b99306661df831
James <Jamestp19@gmail.com>
2023-11-22 10:29:04 -0500

    init with test

---
+++ pic-sure-hpds-ui/src/main/webapp/picsureui/common/contact-widget.js
@@ -0,0 +1,23 @@
+define(["backbone", "handlebars", "text!common/contact-widget.hbs", "picSure/settings"],
+    function (BB, HBS, template, settings) {
+        var contactWidget = BB.View.extend({
+            initialize: function () {
+                this.template = HBS.compile(template);
+            },
+            events: {},
+            render: function () {
+                const key = settings.contactKey && settings.contactKey !== "__CONTACT_KEY__" ? settings.contactKey : false;
+                // if neither analyticsId nor tagManagerId are set, don't render anything
+                if (!key) {
+                    console.log("No contact key set, not rendering contact widget");
+                    // return;
+                }
+
+                this.$el.html(this.template({key: "839c6f3d-d032-4821-ba57-e59ba6e68abd"}));
+            }
+        });
+
+        return {
+            View: contactWidget
+        };
+    });
