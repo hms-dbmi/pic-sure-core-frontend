67d49a237a78c0f41a23e8b3236570f2f2cc589d
Gcolon021 <34667267+Gcolon021@users.noreply.github.com>
2024-09-12 18:05:59 -0400

    Update pic-sure-hpds-ui/src/main/webapp/picsureui/header/header.js

--- pic-sure-hpds-ui/src/main/webapp/picsureui/header/header.js
+++ pic-sure-hpds-ui/src/main/webapp/picsureui/header/header.js
@@ -96,18 +96,6 @@ define([
         },
         logout: function (event) {
             let doLogout = () => {
-                // Attempt to load the logout iFrame for third-party session termination
-                let iframe = document.createElement('iframe');
-                iframe.src = "https://authtest.nih.gov/siteminderagent/smlogoutiframe.asp";
-                iframe.style.display = "none";  // Hide the iframe from view
-                iframe.sandbox = "allow-scripts allow-same-origin";  // Adjust the sandbox if needed
-                document.body.appendChild(iframe);
-
-                // Remove the iframe after 5 seconds
-                setTimeout(() => {
-                    document.body.removeChild(iframe);
-                }, 5000);
-
                 // Save redirection URL so we can log back in after logging out
                 let redirection_url = sessionStorage.redirection_url;
                 sessionStorage.clear();
